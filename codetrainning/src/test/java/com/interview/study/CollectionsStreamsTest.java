55ae875582a119bdee55865afbe63deadb8f2d01
YuriyNik <yuriy.nikitenko@gmail.com>
2025-09-03 17:39:55 +0300

    few collections related tests

---
+++ codetrainning/src/test/java/com/interview/study/CollectionsStreamsTest.java
@@ -0,0 +1,54 @@
+package com.interview.study;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CollectionsStreamsTest {
+
+    @Test
+    void tasksCollectionsStreamsTest(){
+//        Task1 remove duplicates and order by lenght
+        List<String> words = List.of("apple", "banana", "pear", "apple", "kiwi");
+        //        [pear, kiwi, apple, banana]
+        List<String> result = words.stream()
+                .distinct()
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+        System.out.println(result);
+//        Task2 count qty of each char in list.
+        List<String> words2 = List.of("a", "b", "a", "c", "b", "a");
+//        {a=3, b=2, c=1}
+        Map <String, Long> freq = words2.stream().collect(
+                Collectors.groupingBy(
+                        Function.identity(),
+                        Collectors.counting()
+                )
+        );
+        System.out.println(freq);
+// Task3 find max salary for each deparment
+        record Employee(String name, String dept, int salary) {}
+        List<Employee> employees = List.of(
+                new Employee("Alice", "IT", 100),
+                new Employee("Bob", "IT", 200),
+                new Employee("Charlie", "HR", 150),
+                new Employee("Diana", "HR", 50)
+        );
+//        {HR=150, IT=200}
+        Map<String, Integer> maxsalaryByDep = employees.stream().collect(
+            Collectors.groupingBy(
+                    Employee::dept,
+                    Collectors.collectingAndThen(
+                            Collectors.maxBy(Comparator.comparingInt(Employee::salary)),
+                            opt -> opt.get().salary()
+                    )
+            )
+        );
+        System.out.println(maxsalaryByDep);
+
+    }
+}
